1d3e4f8486be9becc1220ada443a7f323eff5871
Kyle Bittinger <kylebittinger@gmail.com>
2015-08-11 16:40:51 -0400

    Use only one cleanup script

--- clean.sh
+++ clean.sh
@@ -5,5 +5,7 @@ rm -Rf dnabc_results/
 rm -Rf illqc_reports/
 rm -Rf illqc_results/
 rm -Rf decontam_results/
+rm -Rf phyloprofiler_results/
+rm -Rf pathfinder_results/
 rm -Rf summary/
 rm -f time.log
--- clean_post_decontam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-rm -Rf phyloprofiler_results/
-rm -Rf pathfinder_results/
-rm -Rf summary/summary-phylo_*
-rm -Rf summary/summary-pathway_*
-
